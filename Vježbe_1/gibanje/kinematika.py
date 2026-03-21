6fe8fad53e350b11094abdca36b19d12da3c3118
lucija <jankoviclucija58@gmail.com>
2026-03-21 16:38:55 +0100

    predana prva vjezba

---
+++ Vježbe_1/gibanje/kinematika.py
@@ -0,0 +1,51 @@
+import matplotlib.pyplot as plt
+
+def jednoliko_gibanje(F, m):
+    t_max = 10
+    dt = 0.01
+    a = F / m
+    
+    t = 0
+    x = 0
+    v = 0
+    
+    vrijeme = []
+    x_lista = []
+    v_lista = []
+    a_lista = []
+    
+    while t <= t_max:
+        vrijeme.append(t)
+        x_lista.append(x)
+        v_lista.append(v)
+        a_lista.append(a)
+        
+        x = x + v * dt
+        v = v + a * dt
+        t = t + dt
+    
+    plt.figure(figsize=(10, 8))
+
+    plt.subplot(3, 1, 1)
+    plt.plot(vrijeme, x_lista)
+    plt.xlabel('t (s)')
+    plt.ylabel('x (m)')
+    plt.title('x - t graf')
+    plt.grid(True)
+
+    plt.subplot(3, 1, 2)
+    plt.plot(vrijeme, v_lista)
+    plt.xlabel('t (s)')
+    plt.ylabel('v (m/s)')
+    plt.title('v - t graf')
+    plt.grid(True)
+
+    plt.subplot(3, 1, 3)
+    plt.plot(vrijeme, a_lista)
+    plt.xlabel('t (s)')
+    plt.ylabel('a (m/s²)')
+    plt.title('a - t graf')
+    plt.grid(True)
+
+    plt.tight_layout()
+    plt.show()  
---
+++ Vježbe_1/gibanje/program.py
@@ -0,0 +1,6 @@
+import kinematika
+
+F = float(input('Unesi silu (N): '))
+m = float(input('Unesi masu (kg): '))
+
+kinematika.jednoliko_gibanje(F,m)
