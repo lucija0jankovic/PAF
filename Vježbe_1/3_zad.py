faeb7e383c4eebd9295cf2b22879e7f1e944dce0
lucija <jankoviclucija58@gmail.com>
2026-03-30 16:39:07 +0200

    izmjenjene vjezben 1

--- Vježbe_1/3_zad.py
+++ Vježbe_1/3_zad.py
@@ -1,24 +1,17 @@
-def unos(poruka):
-    while True:
-        try:
-            broj = float(input(poruka))
-            return broj
-        except:
-            print('unesi broj')
+koordinate = []
 
+for i in ['x1', 'y1', 'x2', 'y2']:
+    unos = input(f'unesi {i} ')
+    while not unos.replace('.', '', 1).isdigit():
+        print('ponovo unesi')
+        unos = input(f'unesi {i} ')
+    koordinate.append(float(unos))
 
-x1 = unos('unesi x1: ')
-y1 = unos('unesi y1: ')
-x2 = unos('unesi x2: ')
-y2 = unos('unesi y2: ')
+x1, y1, x2, y2 = koordinate
 
-while x1 == x2:
-    print('x1 i x2 ne smiju bit isti')
-    x1 = unos('unesi x1: ')
-    x2 = unos('unesi x2: ')
-
-k = (y2 - y1) / (x2 - x1)
-l = y1 - k * x1
-
-print('jednadžba pravca je y =', k, '* x +', l)
-
+if x1 == x2:
+    print(f'jednadzba pravca je x={x1}')
+else:
+    k = (y2 - y1)/(x2 - x1)
+    b = y1 - k * x1
+    print(f'jednadžba pravca y = {k}x + {b}')
